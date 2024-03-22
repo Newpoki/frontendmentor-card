d6eab5f9c1f792716136fbe69e643e5e30237f92
Jason Savelli <jason.savelli@mention.com>
2024-03-22 13:07:54 +0100

    task: tailwind theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,19 @@
 export default {
     content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
     theme: {
+        fontFamily: {
+            sans: ['Space Grotesk', 'Sans-serif'],
+        },
+        colors: {
+            white: '#FFFFFF',
+            grey: {
+                500: '#DEDDDF',
+            },
+            purple: {
+                500: '#8e8593',
+                900: '#21092F',
+            },
+        },
         extend: {},
     },
     plugins: [],
